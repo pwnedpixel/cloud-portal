07e629a339268f924eb03c65af320cee0042f9f4
iYung <ivankhyung@gmail.com>
2019-02-25 17:29:36 -0500

    added login route

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ const app = express();
 var bodyParser = require("body-parser");
 const format = require("util").format;
 var uuid = require("uuid");
+var mysql = require('mysql');
 
 if (process.env.ENVIRO != "PROD") {
     require('dotenv').config()
 }
+
+var connection = mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB
+  });
+
+connection.connect()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,7 +44,13 @@ router
 
 login
     .post("/", (req, res) => {
-        res.send(req.body.user);
+        connection.query("SELECT * FROM USERS WHERE USER_ID = '" + req.body.user + "' AND USER_PASSWORD = '" + req.body.password + "'", function (err, rows, fields) {
+            if (err) throw err
+            if (rows.length > 0) {
+                return res.send("True");
+            }
+            return res.send("False");
+          })
     });
 
 // This will serve the webpage
