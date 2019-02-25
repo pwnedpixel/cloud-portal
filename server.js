b10287c2dc6a62211195a97f82ce24218c6ef48c
iYung <ivankhyung@gmail.com>
2019-02-25 16:57:30 -0500

    added login route

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var port = process.env.PORT || 3003;
+var login = express.Router();
 var router = express.Router();
 
 app.use(function(req, res, next) {
@@ -31,7 +32,13 @@ router
         res.send(req.body.paramname);
     });
 
+login
+    .post("/", (req, res) => {
+        res.send(req.body.user);
+    });
+
 // This will serve the webpage
 app.use(express.static("./public"));
 app.use("/api", router);
+app.use("/login", login);
 app.listen(port, () => console.log("Listening on port " + port));
