b1fd36e8c54cebf5c15a616420bcedcac30fc840
Andy K <andy@akeech.com>
2019-02-28 01:19:28 -0500

    Create VMS

--- server.js
+++ server.js
@@ -101,7 +101,15 @@ user
 
 vm
     .post("/create", (req, res) => {
-        return res.json({success: false});
+        console.log("creating "+req.body.vm_type+" for "+req.body.user_key)
+        connection.query("INSERT INTO `cloudass2`.`VIRTUAL_MACHINES` (`OWNER_ID`, `VM_TYPE`) VALUES ('"+req.body.user_key+"', '"+req.body.vm_type+"')", function (err, rows, fields) {
+            if (err) {
+                return res.json({success: false});
+                throw err
+            } else {
+                return res.json({success: true});
+            }
+          });
     })
     .post("/start", (req, res) => {
         return res.json({success: false});
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+// VIM_IP = "https://cloud-ass-2-vim.herokuapp.com"
+VIM_IP = "http://localhost:3003"
+
 login = async() => {
 	username = document.getElementById("username").value;
 	password = document.getElementById("password").value;
 	body = {user:username,password:password}
 
-	const response = await fetch("https://cloud-ass-2-vim.herokuapp.com/user/login", {
+	const response = await fetch(VIM_IP+"/user/login", {
 	method: 'POST',
     body: JSON.stringify(body), // string or object
     headers:{
@@ -14,7 +17,28 @@ login = async() => {
   if (myJson.success == true) {
 	  document.getElementById("vim").setAttribute("style","")
 	  document.getElementById("login-frame").setAttribute("style","display:none")
+	  user_key = myJson.user_key
+	  populateVMList();
   } else [
 	  alert("Invalid username/password")
   ]
 }
+
+createVM = async(type) => {
+	body = {vm_type:type, user_key:user_key};
+	const response = await fetch(VIM_IP+"/vm/create", {
+	method: 'POST',
+    body: JSON.stringify(body), // string or object
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  });
+  const myJson = await response.json();
+}
+
+populateVMList =() => {
+	vmList = document.getElementById("vms");
+
+}
+
+var user_key = ""
