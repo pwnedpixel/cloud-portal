4a701bd85a7cb7ed74eaf85ab7524bd1615b4809
Andy K <andy@akeech.com>
2019-02-28 00:47:32 -0500

    login working

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-const login = async(event) => {
-	console.log("click")
+login = async() => {
 	username = document.getElementById("username").value;
 	password = document.getElementById("password").value;
-	body = {"user":username, "password": password}
+	body = {user:username,password:password}
 
-	const response = await fetch("https://cloud-ass-2-vim.herokuapp.com/login", {
-    method: 'POST',
-    body: body, // string or object
+	const response = await fetch("https://cloud-ass-2-vim.herokuapp.com/user/login", {
+	method: 'POST',
+    body: JSON.stringify(body), // string or object
     headers:{
       'Content-Type': 'application/json'
     }
   });
   const myJson = await response.json();
-  alert(myJson);
+  if (myJson.success == true) {
+	  document.getElementById("vim").setAttribute("style","")
+	  document.getElementById("login-frame").setAttribute("style","display:none")
+  } else [
+	  alert("Invalid username/password")
+  ]
 }
